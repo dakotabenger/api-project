df67c80d2e0c0fa23f2c6ce0cc40d94fc514078a
Dakota Benger <dakota@bengerlaw.com>
2020-11-25 14:58:14 -0700

    finished

--- routes/tweets.js
+++ routes/tweets.js
@@ -3,7 +3,14 @@ const tweetRouter = express.Router()
 const db = require("../db/models")
 const {Tweet} = db;
 const { check, validationResult } = require('express-validator');
-const tweet = require("../db/models/tweet");
+const validateTweet = [
+    check("message")
+    .exists({checkFalsy: true})
+    .withMessage("Tweet must have message")
+    .isLength({max:280})
+    .withMessage("Tweet must not exceed 280 characters")
+  ]
+
 const handleValidationErrors = (req, res, next) => {
     const validationErrors = validationResult(req);
     if (!validationErrors.isEmpty()) {
@@ -41,16 +48,41 @@ tweetRouter.get("/:id", asyncHandler(async (req,res,next) => {
 
 }))
 
-tweetRouter.post("/tweets", handleValidationErrors, asyncHandler(async(req,res) => {
-   const { message } = req.body.message;
-   check('message').islength({ max: 280 });
+tweetRouter.post("/", validateTweet,handleValidationErrors, asyncHandler(async(req,res) => {
+   console.log(req.body.message)
+    const message  = req.body.message;
     const tweet = await Tweet.create({
-      message
+      'message':message
     })
     res.json({tweet})
 }))
 
+tweetRouter.put("/:id",validateTweet,handleValidationErrors,asyncHandler(async(req,res,next) => {
+    const tweet = await Tweet.findByPk(req.params.id);
+    if (tweet === null) {
+        const error = tweetNotFoundError(req.params.id)
+        next(error)
+    } else {
+        const message = req.body.message
+        const updatedTweet = await tweet.update({"message":message})
+        res.json({updatedTweet})
+        
+    }
+}))
 
+tweetRouter.delete("/:id",handleValidationErrors,asyncHandler(async(req,res,next) => {
+    const tweet = await Tweet.findByPk(req.params.id)
+    console.log(tweet)
+    if (tweet === null) {
+        const error = tweetNotFoundError(req.params.id)
+        next(error)
+    } else {
+        const deletedTweet = await tweet.destroy()
+        res.json({tweet})
+    }  
+    
+    
+}))
 
 module.exports = {
     tweetRouter
